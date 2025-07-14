8d628ac9f8bf423d8796b92e622fe06927d3c6b8
olim007 <olimzhon01@mail.ru>
2025-07-14 09:54:48 +0500

    added git action

--- docs/lecture9/pkg/payment/payment.go
+++ docs/lecture9/pkg/payment/payment.go
@@ -6,7 +6,12 @@ func Payment(cards []types.Card) []types.PaymentSource {
 	pS := []types.PaymentSource{}
 	for _, card := range cards {
 		if card.Active && card.Balance > 0 {
-			pS = append(pS, types.PaymentSource{Type: "card", Number: string(card.Number), Balance: card.Balance})
+			paymentSource := types.PaymentSource{
+				Type: "card", 
+				Number: string(card.Number), 
+				Balance: card.Balance,
+			}
+			pS = append(pS, paymentSource)
 		}
 	}
 	return pS
